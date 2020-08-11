920cc5a07bc69d235edc62e81e54cede0291f115
Kenner Gutierrez <kenneragv@gmail.com>
2020-08-10 22:36:55 -0600

    Firts commit

---
+++ src/components/AddCategory.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+// import  {GifExpertApp} from '../GifExpertApp'
+import PropTypes from 'prop-types'
+
+export const AddCategory = ({setCategories}) => {
+
+    
+    
+    const [inputValue, setinputValue] = useState('');
+    
+    const handleInputChange = (e) => {
+        setinputValue(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(inputValue.trim().length>2){
+            setCategories(c=>[inputValue,...c]);
+            setinputValue('');
+            
+        }
+        
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+            />
+        </form>
+    );
+
+    
+}
+AddCategory.propTypes={
+    setCategories:PropTypes.func.isRequired
+}

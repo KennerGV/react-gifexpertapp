b832877aedad9246ac9966f131358d11d2b779cf
Kenner Gutierrez <kenneragv@gmail.com>
2020-08-10 22:41:25 -0600

    Docs agregado

---
+++ docs/precache-manifest.eaf4fc6662edf199a9e87b718440db89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a68ef63323c475f74e00fea1fdecd150",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5319ca23e6456b1766f",
+    "url": "/static/css/main.27688c5c.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5319ca23e6456b1766f",
+    "url": "/static/js/main.c1a0d681.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
